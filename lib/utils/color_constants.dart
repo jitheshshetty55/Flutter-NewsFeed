e8991ef733d22fc2d368d216051dc6af6eba585d
jitheshshetty55 <jithesh.shetty55@gmail.com>
2020-09-27 11:05:16 +0530

    added color constant values for global widgets

---
+++ lib/utils/color_constants.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+mixin ColorConstants {
+  static const appBackgroundWhite = Color(0xFFFFFFFF);
+  static const primaryColorBlue = Color(0xFF000000);
+  static const accentColorBlack = Color(0xFF000000);
+
+  static const appBarBgBlue = Color(0xFF000000);
+  static const appBarTitleWhite = Color(0xFFFFFFFF);
+  static const appBarMenuBlack = Color(0xFF000000);
+
+  static const textWhite = Color(0xFFFFFFFF);
+  static const textBlack = Color(0xFF000000);
+  static const texttransBlack = Color(0xBE000000);
+  static const textGrey = Color(0xFF666666);
+  static const textLightBlack = Color(0xFF292929);
+  static const textLightGrey = Color(0xFF9E9E9E);
+  static const textLightGrey1 = Color(0xFF333333);
+
+  static const cardIconBlack = Color(0xFF000000);
+
+  static const bottomBarGrey = Color(0x99000000);
+  static const bottomBarBlack = Color(0xFF364997);
+
+  static const tickDisabledGrey = Color(0x99000000);
+  static const tickEnabledBlue = Color(0xFF364997);
+  static const imageBackgroundColor = Color.fromRGBO(245, 245, 245, 1);
+}
